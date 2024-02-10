9912d970ed15806d465b1fc3cc7baa9edfe03c8d
Ryan Wible <ryanwiblecoding@gmail.com>
2024-02-10 09:57:33 -0700

    add test coverage for async retry util

---
+++ tests/utilities/test_async_retry.py
@@ -0,0 +1,47 @@
+import pytest
+
+from wibbley.utilities.async_retry import AsyncRetry
+
+
+class FakeClass:
+    def __init__(self):
+        self.attempts = 0
+
+    async def handle(self):
+        self.attempts += 1
+
+
+@pytest.mark.asyncio
+async def test__async_retry__retries_up_to_max_attempts():
+    # Arrange
+    async_retry = AsyncRetry(max_attempts=3, base_delay=0)
+    attempts = 0
+
+    @async_retry
+    async def fake_function():
+        nonlocal attempts
+        attempts += 1
+        raise ValueError("Fake error")
+
+    # Act
+    try:
+        await fake_function()
+    except ValueError:
+        pass
+
+    # Assert
+    assert attempts == 3
+
+
+@pytest.mark.asyncio
+async def test__async_retry__reraises_on_last_attempt():
+    # Arrange
+    async_retry = AsyncRetry(max_attempts=1, base_delay=0)
+
+    @async_retry
+    async def fake_function():
+        raise ValueError("Fake error")
+
+    # Act/Assert
+    with pytest.raises(ValueError):
+        await fake_function()
